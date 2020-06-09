cd878d461cd1c895040b045b5c84838509f9b356
caique camargo <caique.de.camargo@hotmail.com>
2020-06-09 09:27:19 -0300

    waiting for rfid cards to test

--- access_control/access_control.ino
+++ access_control/access_control.ino
@@ -6,28 +6,49 @@
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 
-static int lenUID = 40;
-String validUID[lenUID];
+#define LEN_UID 2
+String *validUID;
+
+#define PORT_TRIGGER 9
  
 void setup() 
 {
-  Serial.begin(9600);   // Inicia a serial
+  Serial.begin( 9600 );   // Inicia a serial
   SPI.begin();      // Inicia  SPI bus
+  
   mfrc522.PCD_Init();   // Inicia MFRC522
-  Serial.println("Aproxime o seu cartao do leitor...");
+  createValidUIDTable();
+
+  pinMode( PORT_TRIGGER, OUTPUT );
+  digitalWrite( PORT_TRIGGER, LOW );
+  
+  Serial.println( "Aproxime o seu cartao do leitor..." );
   Serial.println();
 }
 
 void createValidUIDTable(){
-  validUID[0] = "79 3A DB A2";
+  String validUID_temp[] = {
+    "79 3A DB A2",
+    "79 3A DB A3"
+  };
+  validUID = validUID_temp;
 }
 
-void isValidUID( uid ){
-  for (int i = 0; i < lenUID; i++){
-     if (validUID[i] == lenUID) return 1;
+int isValidUID( String uid ){
+  for ( int i = 0; i < LEN_UID; i++ ){
+     if ( validUID[i] == uid ) return 1;
   }
 
   return 0;
+}
+
+void openTheDoor(){
+  Serial.println( "Porta Aberta" );
+  digitalWrite( PORT_TRIGGER, HIGH );
+}
+
+void closeTheDoor(){
+  digitalWrite( PORT_TRIGGER, LOW );
 }
  
 void loop() 
@@ -42,18 +63,26 @@ void loop()
   {
     return;
   }
+  
   //Mostra UID na serial
-  Serial.print("UID da tag :");
-  String conteudo= "";
-  byte letra;
+  Serial.print( "UID da tag :" );
+  String uid= "";
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
-     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial.print(mfrc522.uid.uidByte[i], HEX);
-     conteudo.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
+     uid.concat( String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ") );
+     //Serial.print(i);
+     //Serial.print(" ");
+     //Serial.println(conteudo);
+     uid.concat( String(mfrc522.uid.uidByte[i], HEX) );
+     //Serial.print(i);
+     //Serial.print(" ");
+     //Serial.println(conteudo);
   }
-  Serial.println();
-  Serial.println(conteudo);
-  conteudo.toUpperCase();
+  uid.toUpperCase();
+  Serial.println( uid );
+  if ( isValidUID( uid ) ){
+    openTheDoor();
+    delay(1000);
+    closeTheDoor();
+  }
 } 
